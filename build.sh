c96b59b7bb557d6e5b79d5b502ee46cdcdbbd7a5
ShinJAE3 <naezang@gmail.com>
2017-09-25 13:40:58 +0900

    add spi

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 make -j
-cp arch/arm/boot/zImage-dtb ../
+
+cp arch/arm/boot/zImage-dtb ../zImage-dtb_$(date +"%Y-%m-%d")
+cp ../zImage-dtb_$(date +"%Y-%m-%d") ~/workspace/git/xu4android/
+cp ../zImage-dtb_$(date +"%Y-%m-%d") ~/Share/
